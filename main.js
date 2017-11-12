42fcdd4ff5fd40d0296137f58470b1a893e2389b
Nelson Gomes <nelsonmpg@gmail.com>
2017-11-12 15:49:21 +0000

    Inicio do projeto

---
+++ main.js
@@ -0,0 +1,16 @@
+require('colors');
+var cp = require('child_process'),
+	fs = require('fs');
+	
+var Main = function () {
+  var args = {
+      port: 8080
+    };
+    // inicia p script e envia as configuracores do ficheiro ini
+    var child2 = cp.fork('./lib/server.js');
+    child2.send({"serverdata" : args});
+};
+
+new Main();
+
+module.exports = Main;
---
+++ www/js/script.js
@@ -0,0 +1,55 @@
+
+var socket = io();
+
+$(document).ready(function(){
+
+	$(".btn-menu").click(function(){
+		$(".btn-menu").removeClass("btn-info").addClass("btn-success");
+		$(this).removeClass("btn-success").addClass("btn-info");		
+	});
+		
+	$("body").keypress(function(e){			
+		socket.emit('keypress', e.which);		
+	});
+	
+	socket.on('keypress', function(key){
+		console.log("received: ", key);
+		
+		if(key === 97) {
+			var SelElent = checkIndexSelk();			
+			if(SelElent.index === 0){
+				$(SelElent.elements[SelElent.size]).click();
+			} else {
+				$(SelElent.elements[SelElent.index - 1]).click();			
+			}			
+		}		
+		if(key === 115) {
+			var SelElent = checkIndexSelk();			
+			if(SelElent.index === SelElent.size){
+				$(SelElent.elements[0]).click();
+			} else {
+				$(SelElent.elements[SelElent.index + 1]).click();			
+			}						
+		}
+		
+		if(key >= 49 && key <= 53){
+			 $("#btn-menu-" + (key - 48)).click();
+		}
+	});
+});
+
+var checkIndexSelk = function(){
+	var allbtn = $(".btn-menu");
+	var indexSel = {
+		elements: allbtn,
+		size: allbtn.length - 1,
+		index: 0
+	};		
+	for(var i = 0; i <= indexSel.size; i++){
+		if($(allbtn[i]).hasClass("btn-info")){
+			indexSel.index = i;
+			break;
+		}
+	}
+	return indexSel;
+}
