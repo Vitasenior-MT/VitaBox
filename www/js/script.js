e72555e8fce313d5bf61a3af2c500484713404c1
Nelson Gomes <nelsonmpg@gmail.com>
2017-12-10 16:25:55 +0000

    no message

--- www/js/script.js
+++ www/js/script.js
@@ -61,34 +61,34 @@ $(document).ready(function(){
 
 
 
-	$("body").keypress(function(e){			
-		socket.emit('keypress', e.which);		
-	});
+	// $("body").keypress(function(e){			
+	// 	socket.emit('keypress', e.which);		
+	// });
 	
-	socket.on('keypress', function(key){
-		console.log("received: ", key);
+	// socket.on('keypress', function(key){
+	// 	console.log("received: ", key);
 		
-		if(key === 97) {
-			var SelElent = checkIndexSelk();			
-			if(SelElent.index === 0){
-				$(SelElent.elements[SelElent.size]).click();
-			} else {
-				$(SelElent.elements[SelElent.index - 1]).click();			
-			}			
-		}		
-		if(key === 115) {
-			var SelElent = checkIndexSelk();			
-			if(SelElent.index === SelElent.size){
-				$(SelElent.elements[0]).click();
-			} else {
-				$(SelElent.elements[SelElent.index + 1]).click();			
-			}						
-		}
+	// 	if(key === 97) {
+	// 		var SelElent = checkIndexSelk();			
+	// 		if(SelElent.index === 0){
+	// 			$(SelElent.elements[SelElent.size]).click();
+	// 		} else {
+	// 			$(SelElent.elements[SelElent.index - 1]).click();			
+	// 		}			
+	// 	}		
+	// 	if(key === 115) {
+	// 		var SelElent = checkIndexSelk();			
+	// 		if(SelElent.index === SelElent.size){
+	// 			$(SelElent.elements[0]).click();
+	// 		} else {
+	// 			$(SelElent.elements[SelElent.index + 1]).click();			
+	// 		}						
+	// 	}
 		
-		if(key >= 49 && key <= 53){
-			 $("#btn-menu-" + (key - 48)).click();
-		}
-	});
+	// 	if(key >= 49 && key <= 53){
+	// 		 $("#btn-menu-" + (key - 48)).click();
+	// 	}
+	// });
 
 	socket.on("ready", function(data){
 		console.log("ready", data);
