623d73d655b4ca86f6361680d10bdb2a4f2f6cc3
Nelson Gomes <nelsonmpg@gmail.com>
2017-12-10 17:09:22 +0000

    no message

--- www/js/script.js
+++ www/js/script.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 	});
 
 	socket.on('msgOutput', function(output){
-		console.log("output", output);
+		// console.log("output", output);
 		$('#outputTextarea').val(function(i, text) {
 		    return text + output + "\n";
 		});
@@ -73,30 +73,30 @@ $(document).ready(function(){
 	// 	socket.emit('keypress', e.which);		
 	// });
 	
-	// socket.on('keypress', function(key){
-	// 	console.log("received: ", key);
+	socket.on('keypress', function(key){
+		console.log("received: ", key);
 		
-	// 	if(key === 97) {
-	// 		var SelElent = checkIndexSelk();			
-	// 		if(SelElent.index === 0){
-	// 			$(SelElent.elements[SelElent.size]).click();
-	// 		} else {
-	// 			$(SelElent.elements[SelElent.index - 1]).click();			
-	// 		}			
-	// 	}		
-	// 	if(key === 115) {
-	// 		var SelElent = checkIndexSelk();			
-	// 		if(SelElent.index === SelElent.size){
-	// 			$(SelElent.elements[0]).click();
-	// 		} else {
-	// 			$(SelElent.elements[SelElent.index + 1]).click();			
-	// 		}						
-	// 	}
+		if(key === 97) {
+			var SelElent = checkIndexSelk();			
+			if(SelElent.index === 0){
+				$(SelElent.elements[SelElent.size]).click();
+			} else {
+				$(SelElent.elements[SelElent.index - 1]).click();			
+			}			
+		}		
+		if(key === 115) {
+			var SelElent = checkIndexSelk();			
+			if(SelElent.index === SelElent.size){
+				$(SelElent.elements[0]).click();
+			} else {
+				$(SelElent.elements[SelElent.index + 1]).click();			
+			}						
+		}
 		
-	// 	if(key >= 49 && key <= 53){
-	// 		 $("#btn-menu-" + (key - 48)).click();
-	// 	}
-	// });
+		if(key >= 49 && key <= 53){
+			 $("#btn-menu-" + (key - 48)).click();
+		}
+	});
 
 	socket.on("ready", function(data){
 		console.log("ready", data);
@@ -104,6 +104,12 @@ $(document).ready(function(){
 
 	socket.on("status", function(data){
 		console.log("status", data);
+
+		$('#outputTextarea').val(function(i, text) {
+		    return text + JSON.stringify(data, undefined, 2) + "\n";
+		});
+		$('#outputTextarea').scrollTop($('#outputTextarea')[0].scrollHeight);
+
 	});
 
 	socket.on("datakey", function(data){
