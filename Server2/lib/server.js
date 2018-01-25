15d1e22d52721949dec885c4e9743e934f5c52d3
Nelson Gomes <nelsonmpg@gmail.com>
2018-01-25 23:58:22 +0000

    Add new server to teste insert values.

--- Server2/lib/server.js
+++ Server2/lib/server.js
@@ -3,24 +3,20 @@ require('colors'); //bold, italic, underline, inverse, yellow, cyan, white, mage
 var express = require('express'),
 	http = require('http'),
 	fs = require('fs'),
-	socketio = require('socket.io'),
-	//serverIo = require('./serverSkt'),
 	bodyParser = require('body-parser'),
-	cp = require('child_process'),
-	snrlib = require('./seensorlib.js');
+	cp = require('child_process');
 
 var Server = function (config) {
   this.app = express();
   this.server = http.Server(this.app);
-  //this.io = socketio(this.server);
   this.port = config.port;
+	this.remoteserver = config.remoteserver;
+	this.remoteport = config.remoteport;
 };
 
 Server.prototype.start = function () {
 	var self = this;
 	self.server.listen(self.port);
-	//self.skt = new serverIo({server: self});
-	//self.skt.init();
 
 	var allowCrossDomain = function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
@@ -37,9 +33,47 @@ Server.prototype.start = function () {
 	// fornece ao cliente a pagina index.html
 	this.app.use(express.static(__dirname + './../www'));
 
-this.app.post("/api/sensor", snrlib.postSensor);
-this.app.post("/api/sensor2", snrlib.postSensor);
+	this.app.post("/app/sensor", (req, res) => {
+		var self = this;
+		console.log("teste req", req.body);
+		var jsonObject = JSON.stringify(req.body);
 
+		var options = {
+				host: this.remoteserver,
+				port: this.remoteport,
+				path: '/api/sensor',
+				method: 'POST',
+				headers: {
+						'Content-Type': 'application/json',
+						'Content-Length': Buffer.byteLength(jsonObject)
+				}
+		};
+		// console.log(options);
+		try {
+				var req = http.request(options, function(res) {
+						console.log('STATUS: ' + res.statusCode);
+						res.setEncoding('utf8');
+						var responseString = '';
+						res.on('data', function(data) {
+								responseString += data;
+						});
+						res.on('end', function() {
+								console.log("Receive - ", responseString);
+						});
+				});
+				req.write(jsonObject);
+				req.end();
+				req.on('error', function(e) {
+						console.error("Error -> ", e);
+				});
+		} catch (e) {
+				console.log("Erro ao tentar ligar ao servidor remoto!!!", e)
+		}
+
+		return res.json({
+      status : true
+    });
+	});
 
 	console.log('\nServer HTTP Wait %d'.green.bold, self.port);
 }
