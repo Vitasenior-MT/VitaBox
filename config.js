98aaee705bc2fb4613f5a77dc92a4cad3b892fb1
dariojorge <jorge12dario12@hotmail.com>
2018-03-06 17:06:01 +0000

    fixes

--- config.js
+++ config.js
@@ -4,8 +4,12 @@ module.exports = {
   ServerBoardListenerConfigs: {
     port: 10000,
     host: 'fd00::1',
-    remoteserver: "192.168.1.63",
+    remoteserver: "192.168.161.132",
     remoteport: 8080
+  },
+  ServerConfigs: {
+    port: 8080,
+    host: '192.168.161.151'
   },
   serverHttp: {
     port: 8080
