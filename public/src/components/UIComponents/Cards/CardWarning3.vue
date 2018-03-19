3eba22999923f5a9e06e8b35632518a890589bd2
dariojorge <jorge12dario12@hotmail.com>
2018-03-19 12:50:44 +0000

    fix

---
+++ public/src/components/UIComponents/Cards/CardWarning3.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='card'>
+    <div class='content'>
+        <div class='row'>
+          <div class="col-md-12">
+            <div class="card card-plain">
+              <paper-table type="hover" :title="warningCard.location" :sub-title="''" :data="warningCard.sensors" :columns="this.columns">
+              </paper-table>
+            </div>
+          </div>
+        </div>
+      <div class='footer'>
+    </div>
+      </div>
+  </div>
+</template>
+<script>
+import PaperTable from 'components/UIComponents/PaperTable2.vue'
+export default {
+  components: {
+    PaperTable
+  },
+  name: 'Warning',
+  data() {
+    return {
+      columns: ['SensorType', 'Avg', 'Threshold', 'AvgLastUpdate']
+    }
+  },
+  props: {
+    warningCard: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {}
+}
+</script>
+<style>
+</style>
