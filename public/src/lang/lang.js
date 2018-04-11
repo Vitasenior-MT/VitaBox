5f3188e73605983114896603461f240cdf3c48f3
dariojorge <jorge12dario12@hotmail.com>
2018-04-11 15:35:45 +0100

    fix no i18n que não trocava de lingua, ja o faz

--- public/src/lang/lang.js
+++ public/src/lang/lang.js
@@ -2,16 +2,15 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 import pt from './pt.json'
-// import en from './en.json'
-// import tw from './tw.json'
-// import es from './es.json'
+import en from './en.json'
 
 Vue.use(VueI18n)
 
 const locale = 'pt'
 
 const messages = {
-  pt
+  pt,
+  en,
 }
 
 const i18n = new VueI18n({
