61e92548bfc697d482df09e90d7216bf4b489930
Nelson Gomes <nelsonmpg@gmail.com>
2018-04-17 00:21:34 +0100

    Big fix

--- public/src/components/Dashboard/Views/Exams.vue
+++ public/src/components/Dashboard/Views/Exams.vue
@@ -4,34 +4,10 @@
       <div class="card">
         <div class="content">
           <div class="row">
-            <div class="col-md-2">
-              <button class="btn btn-block control-remote" type="button" data-type="bloodpressure" v-on:click="bloodpressure">
-                <h3><b class="ti-heart-broken"></b></h3>
-                <h5>Pressão Arterial</h5>
-              </button>
-            </div>
-            <div class="col-md-2">
-              <button class="btn btn-block control-remote" type="button" data-type="bodyscale" v-on:click="bodyscale">
-                <h3><b class="ti-dashboard"></b></h3>
-                <h5>Pesar</h5>
-              </button>
-            </div>
-            <div class="col-md-2">
-              <button class="btn btn-block control-remote" type="button" data-type="bloodglucose" v-on:click="bloodglucose">
-                <h3><b class="ti-bar-chart-alt"></b></h3>
-                <h5>Glicemia</h5>
-              </button>
-            </div>
-            <div class="col-md-2">
-              <button class="btn btn-block control-remote" type="button" data-type="bodyTemperature" v-on:click="bodyTemperature">
-                <h3><b class="ti-ruler-alt"></b></h3>
-                <h5>Temperatura</h5>
-              </button>
-            </div>
-            <div class="col-md-2">
-              <button class="btn btn-block control-remote" type="button" data-type="bodyPulse" v-on:click="bodyPulse">
-                <h3><b class="ti-heart-broken"></b></h3>
-                <h5>Pulsometro</h5>
+            <div class="col-md-2" v-for="btn in btnExams"  :key='btn.id'>
+              <button class="btn btn-block control-remote" type="button" :data-type="btn.type" v-on:click="bleExecExam">
+                <h3><b :class="btn.icon"></b></h3>
+                <h5>{{ btn.nome }}</h5>
               </button>
             </div>
           </div>
@@ -85,43 +61,76 @@
           </div>
         </div>
         <div class="col-md-4">
-          <stats-card>
-            <div class="icon-big text-center" slot="header">
-              <b class="ti-stats-up"></b>
+          <div class="card">
+            <div class="content">
+              <div class="row">
+                <div class="col-xs-5">
+                  <div class="icon-big text-center">
+                    <i class="ti-stats-up"></i>
+                  </div>
+                </div>
+                <div class="col-xs-7">
+                  <div class="numbers">
+                    <p>Máxima</p>
+                    {{dataPressArt.pressmax}}
+                  </div>
+                </div>
+              </div>
+              <div class="footer">
+                <hr/>
+                <div class="stats" slot="footer">
+                </div>
+              </div>
             </div>
-            <div class="numbers" slot="content">
-              <p>Máxima</p>
-              {{dataPressArt.pressmax}}
-            </div>
-            <div class="stats" slot="footer">
-            </div>
-          </stats-card>
+          </div>
         </div>
         <div class="col-md-4">
-          <stats-card>
-            <div class="icon-big text-center" slot="header">
-              <b class="ti-stats-down"></b>
+          <div class="card">
+            <div class="content">
+              <div class="row">
+                <div class="col-xs-5">
+                  <div class="icon-big text-center">
+                    <i class="ti-stats-down"></i>
+                  </div>
+                </div>
+                <div class="col-xs-7">
+                  <div class="numbers">
+                    <p>Minima</p>
+                    {{dataPressArt.pressmin}}
+                  </div>
+                </div>
+              </div>
+              <div class="footer">
+                <hr/>
+                <div class="stats" slot="footer">
+                </div>
+              </div>
             </div>
-            <div class="numbers" slot="content">
-              <p>Minima</p>
-              {{dataPressArt.pressmin}}
-            </div>
-            <div class="stats" slot="footer">
-            </div>
-          </stats-card>
+          </div>
         </div>
         <div class="col-md-4">
-          <stats-card>
-            <div class="icon-big text-center" slot="header">
-              <b class="ti-pulse"></b>
+          <div class="card">
+            <div class="content">
+              <div class="row">
+                <div class="col-xs-5">
+                  <div class="icon-big text-center">
+                    <i class="ti-pulse"></i>
+                  </div>
+                </div>
+                <div class="col-xs-7">
+                  <div class="numbers">
+                    <p>Pulso Minimo</p>
+                    {{dataPressArt.pulso}}
+                  </div>
+                </div>
+              </div>
+              <div class="footer">
+                <hr/>
+                <div class="stats" slot="footer">
+                </div>
+              </div>
             </div>
-            <div class="numbers" slot="content">
-              <p>Pulso Minimo</p>
-              {{dataPressArt.pulso}}
-            </div>
-            <div class="stats" slot="footer">
-            </div>
-          </stats-card>
+          </div>
         </div>
       </div>
     </div>
@@ -233,7 +242,7 @@
         </div>
       </div>
     </div>
-    <div class="row" v-if="examEvent == 'bodyTemperature'">
+    <div class="row" v-if="examEvent == 'bodytemperature'">
       <div class="col-md-12">
         <div class="col-md-8">
           <div class="card">
@@ -276,38 +285,60 @@
           </div>
         </div>
           <div class="col-md-4">
-            <stats-card>
-              <div class="icon-big text-center" slot="header">
-                <i v-if="battery >= 100" class="fas fa-battery-full"></i>
-                <i v-else-if="battery >= 75 && battery < 100" class="fas fa-battery-three-quarters"></i>
-                <i v-else-if="battery >= 50 && battery < 75" class="fas fa-battery-half"></i>
-                <i v-else-if="battery >= 25 && battery < 50" class="fas fa-battery-quarter"></i>
-                <i v-else class="fas fa-battery-empty"></i>
+            <div class="card">
+              <div class="content">
+                <div class="row">
+                  <div class="col-xs-5">
+                    <div class="icon-big text-center">
+                      <i v-if="battery >= 100" class="fas fa-battery-full"></i>
+                      <i v-else-if="battery >= 75 && battery < 100" class="fas fa-battery-three-quarters"></i>
+                      <i v-else-if="battery >= 50 && battery < 75" class="fas fa-battery-half"></i>
+                      <i v-else-if="battery >= 25 && battery < 50" class="fas fa-battery-quarter"></i>
+                      <i v-else class="fas fa-battery-empty"></i>
+                    </div>
+                  </div>
+                  <div class="col-xs-7">
+                    <div class="numbers">
+                      <p>Bateria</p>
+                      {{battery}}%
+                    </div>
+                  </div>
+                </div>
+                <div class="footer">
+                  <hr/>
+                  <div class="stats" slot="footer">
+                  </div>
+                </div>
               </div>
-              <div class="numbers" slot="content">
-                <p>Bateria</p>
-                {{battery}}%
-              </div>
-              <div class="stats" slot="footer">
-              </div>
-            </stats-card>
+            </div>
           </div>
           <div class="col-md-4">
-            <stats-card>
-              <div class="icon-big text-center" slot="header">
-                <i class="fas fa-thermometer"></i>
+            <div class="card">
+              <div class="content">
+                <div class="row">
+                  <div class="col-xs-5">
+                    <div class="icon-big text-center">
+                      <i class="fas fa-thermometer"></i>
+                    </div>
+                  </div>
+                  <div class="col-xs-7">
+                    <div class="numbers">
+                      <p>Temperatura Corporal</p>
+                      {{tempCorp}}ºC
+                    </div>
+                  </div>
+                </div>
+                <div class="footer">
+                  <hr/>
+                  <div class="stats" slot="footer">
+                  </div>
+                </div>
               </div>
-              <div class="numbers" slot="content">
-                <p>Temperatura Corporal</p>
-                {{tempCorp}}ºC
-              </div>
-              <div class="stats" slot="footer">
-              </div>
-            </stats-card>
+            </div>
           </div>
       </div>
     </div>
-    <div class="row" v-if="examEvent == 'bodyPulse'">
+    <div class="row" v-if="examEvent == 'bodypulse'">
       <div class="col-md-12">
         <div class="col-md-8">
           <div class="card">
@@ -348,30 +379,52 @@
           </div>
         </div>
           <div class="col-md-4">
-            <stats-card>
-              <div class="icon-big text-center" slot="header">
-                <b class="ti-shine"></b>
+            <div class="card">
+              <div class="content">
+                <div class="row">
+                  <div class="col-xs-5">
+                    <div class="icon-big text-center">
+                      <i class="fab fa-ioxhost"></i>
+                    </div>
+                  </div>
+                  <div class="col-xs-7">
+                    <div class="numbers">
+                      <p>Oxigénio</p>
+                      {{spoVal}}%
+                    </div>
+                  </div>
+                </div>
+                <div class="footer">
+                  <hr/>
+                  <div class="stats" slot="footer">
+                  </div>
+                </div>
               </div>
-              <div class="numbers" slot="content">
-                <p>Oxigénio</p>
-                {{spoVal}}%
-              </div>
-              <div class="stats" slot="footer">
-              </div>
-            </stats-card>
+            </div>
           </div>
           <div class="col-md-4">
-            <stats-card>
-              <div class="icon-big text-center" slot="header">
-                <b class="ti-heart-broken"></b>
+            <div class="card">
+              <div class="content">
+                <div class="row">
+                  <div class="col-xs-5">
+                    <div class="icon-big text-center">
+                      <i class="fas fa-heartbeat"></i>
+                    </div>
+                  </div>
+                  <div class="col-xs-7">
+                    <div class="numbers">
+                      <p>Pulso</p>
+                      {{pulseVal}} PPM
+                    </div>
+                  </div>
+                </div>
+                <div class="footer">
+                  <hr/>
+                  <div class="stats" slot="footer">
+                  </div>
+                </div>
               </div>
-              <div class="numbers" slot="content">
-                <p>Pulso</p>
-                {{pulseVal}}%
-              </div>
-              <div class="stats" slot="footer">
-              </div>
-            </stats-card>
+            </div>
           </div>
       </div>
     </div>
@@ -397,15 +450,18 @@
 </template>
 <script>
 import { EventBus } from '../../../event-bus.js'
-import StatsCard from 'components/UIComponents/Cards/StatsCard.vue'
 import ChartGauge from 'components/UIComponents/Charts/chartGaugeItem1.vue'
 export default {
   components: {
-    ChartGauge,
-    StatsCard
+    ChartGauge
   },
   data() {
     return {
+      btnExams: [{
+        nome: '',
+        type: '',
+        icon: ''
+      }],
       dataPressArt: {
         // definição do ojecto para medir a pressão arterial
         id: 'pressArterial',
@@ -426,9 +482,10 @@ export default {
   },
   sockets: {
     bleExecFimPulse: function(data) {
+      console.log('Pulse', data)
       if (data.satus === true) {
-        this.spoVal = data.data.readInt8(3)
-        this.pulseVal = data.readInt8(4)
+        this.spoVal = data.data.spo2
+        this.pulseVal = data.data.pulse
       } else {
         this.spoVal = 0
         this.pulseVal = 0
@@ -463,7 +520,7 @@ export default {
       // console.log('BleMsg', data)
       if (data.satus === true) {
         this.$notifications.notify({
-          message: data.data,
+          message: '<h3>' + data.data + '</h3>',
           icon: 'ti-bell',
           horizontalAlign: 'right',
           verticalAlign: 'top',
@@ -472,12 +529,13 @@ export default {
       } else {
         console.log('Receive error', data)
         this.$notifications.notify({
-          message: 'Erro recebido.<br>' + data.data,
+          message: '<h3><b>Erro!</b><br>' + data.data + '</h3>',
           icon: 'ti-bell',
           horizontalAlign: 'right',
           verticalAlign: 'top',
           type: 'danger'
         })
+        this.execProcess = false
       }
     },
     /**
@@ -493,7 +551,7 @@ export default {
       } else {
         console.log('Receive error', data)
         this.$notifications.notify({
-          message: 'Erro recebido.<br>' + data.data,
+          message: '<h3><b>Erro!</b><br>' + data.data + '</h3>',
           icon: 'ti-bell',
           horizontalAlign: 'left',
           verticalAlign: 'top',
@@ -515,7 +573,7 @@ export default {
       } else {
         console.log('Receive error', data)
         this.$notifications.notify({
-          message: 'Erro recebido.<br>' + data.data,
+          message: '<h3><b>Erro!</b><br>' + data.data + '</h3>',
           icon: 'ti-bell',
           horizontalAlign: 'left',
           verticalAlign: 'top',
@@ -530,7 +588,7 @@ export default {
     bleError: function(data) {
       console.log('bleError', data)
       this.$notifications.notify({
-        message: 'Erro recebido.<br>' + data.data,
+        message: '<h3><b>Erro!</b><br>' + data.data + '</h3>',
         icon: 'ti-bell',
         horizontalAlign: 'left',
         verticalAlign: 'top',
@@ -539,79 +597,18 @@ export default {
     }
   },
   methods: {
-    /**
-     * TODO: Metodo para iniciar o processo de mediação da pressão arterial
-     */
-    bloodpressure() {
+    bleExecExam() {
       this.execProcess = true
       this.dataPressArt.val = 0
       this.dataPressArt.pressmax = 0
       this.dataPressArt.pressmin = 0
       this.dataPressArt.pulso = 0
-      this.$http
-        .get('/api/ble/bloodPressure')
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log('----> ', error)
-          this.data = error
-        })
-    },
-    /**
-     * TODO: Metodo para iniciar o processo de efetuar a pesagem
-     */
-    bodyscale() {
-      this.execProcess = true
-      this.$http
-        .get('/api/ble/bodyscale')
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log('----> ', error)
-          this.data = error
-        })
-    },
-    /**
-     * TODO: Metodo para iniciar o processo de medição da glucose
-     */
-    bloodglucose() {
-      this.execProcess = true
-      this.$http
-        .get('/api/ble/bloodglucose')
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log('----> ', error)
-          this.data = error
-        })
-    },
-    /**
-     * TODO: Metodo para iniciar o processo de medição da temperatura
-     */
-    bodyTemperature() {
-      this.execProcess = true
       this.battery = 0
       this.tempCorp = 0
+      this.spoVal = 0
+      this.pulseVal = 0
       this.$http
-        .get('/api/ble/bodytemperature')
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log('----> ', error)
-          this.data = error
-        })
-    },
-    /**
-     * TODO: Metodo para iniciar o processo de medição da pulso
-     */
-    bodyPulse() {
-      this.execProcess = true
-      this.$http
-        .get('/api/ble/bodyPulse')
+        .get('/api/ble/' + this.examEvent.toLowerCase())
         .then(response => {
           console.log(response)
         })
@@ -654,11 +651,11 @@ export default {
               break
             case 'right': // tecla para a direita
               EventBus.moveLeftRightInView(1)
-              self.examEvent = EventBus.elementControl[EventBus.currentActiveRightComp].dataset.type
+              self.examEvent = EventBus.elementControl[EventBus.currentActiveRightComp].dataset.type.toLowerCase()
               break
             case 'left': // tecla para a esquerda
               EventBus.moveLeftRightInView(-1)
-              self.examEvent = EventBus.elementControl[EventBus.currentActiveRightComp].dataset.type
+              self.examEvent = EventBus.elementControl[EventBus.currentActiveRightComp].dataset.type.toLowerCase()
               break
             default:
               break
@@ -668,10 +665,34 @@ export default {
     }
   },
   created() {
+    let btns = [
+      {
+        nome: 'Pressão Arterial',
+        type: 'bloodpressure',
+        icon: 'ti-heart-broken'
+      }, {
+        nome: 'Temperatura',
+        type: 'bodyTemperature',
+        icon: 'ti-ruler-alt'
+      }, {
+        nome: 'Pulsometro',
+        type: 'bodyPulse',
+        icon: 'ti-heart-broken'
+      }, {
+        nome: 'Pesar',
+        type: 'bodyscale',
+        icon: 'ti-dashboard'
+      }, {
+        nome: 'Glicemia',
+        type: 'bloodglucose',
+        icon: 'ti-bar-chart-alt'
+      }]
+    this.btnExams = btns
     // Consulta o DOM HTML por todos os elemento pertencentes à class 'control-remote'
     this.controlEventsBus()
     // console.log('Remotes', EventBus.elementControl)
   },
+  beforeCreate() {},
   /**
    * TODO: Destroi o evento das teclas do comando para esta view
    */
