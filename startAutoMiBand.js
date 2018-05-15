659a9995a9212a327da80af2d15da1198517b138
Nelson Gomes <nelsonmpg@gmail.com>
2018-05-15 23:59:14 +0100

    Criação do script para executr o modo automático da mi band e o script em bash .

---
+++ startAutoMiBand.js
@@ -0,0 +1,72 @@
+'use strict'
+require('colors');
+var cp = require('child_process'),
+  fs = require('fs'),
+  connectDB = require('./lib/connectDB.js'),
+  patientslib = require('./lib/patientslib.js'),
+  rawsensorlib = require('./lib/rawsensorlib.js'),
+  log = require('./lib/logger'),
+  errorLog = log.errorlog,
+  successlog = log.successlog,
+  config = null,
+  mode = process.env.NODE_ENV || "DEV";
+
+var Main = function () {
+  let pathCfg = './config-' + mode.toLowerCase() + '/config.js';
+  successlog.info('Path Config -', pathCfg);
+  config = require(pathCfg);
+  this.mongodbConfig = config.mongodb;
+  this.allPatientes = [];
+};
+
+Main.prototype.start = function () {
+  var self = this;
+  connectDB.connectDB(this.mongodbConfig, function () {
+    successlog.info(`DataBase is up.`);
+    patientslib.getAllPatientsIdMiBandDevice(function (patients) {
+      self.allPatientes = patients;
+      if (self.allPatientes.length > 0) {
+        self.execBleMiBand(self.allPatientes.pop());
+      } else {
+        console.log("process End");
+        process.exit(0);
+      }      
+    })
+  });
+}
+
+Main.prototype.execBleMiBand = function (pacientInfo) {
+  var self = this;
+  var args = {
+    options: {
+      bandfitness_auth: pacientInfo.bandfitness_auth
+    },
+    pacientId: pacientInfo.userId,
+    action: pacientInfo.BoardmodelName,
+    macAddress: pacientInfo.mac_addr,
+    boardId: pacientInfo.boardId,
+    deviceId: pacientInfo.deviceId,
+    BoardmodelId: pacientInfo.BoardmodelId,
+    sensors: pacientInfo.Sensors
+  };
+  // inicia p script e envia as configuracores do ficheiro inicial
+  var child = cp.fork('./lib/bleServer.js');
+  child.send({ "serverdata": args });
+  child.on('message', function (data) {
+    if (data.proc === 'saveDataSensors') {
+      // console.log(data);
+      rawsensorlib.insertManyData(data.dataSend);
+    } else if (data.proc === 'saveAuthPatient') {
+      patientslib.updateFlagAuthBandFit(data.dataSend);
+    }
+  });
+  child.on('exit', function (data) {
+    console.log("process End");
+    process.exit(0);
+  })
+}
+
+var m = new Main();
+m.start();
+
+module.exports = Main;
