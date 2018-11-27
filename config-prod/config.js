e784344806ca6367a0f60177d0c71a35784f5c37
dariojorge <jorge12dario12@hotmail.com>
2018-11-27 12:07:22 +0000

    changes

--- config-prod/config.js
+++ config-prod/config.js
@@ -59,8 +59,8 @@ module.exports = {
     host: 'fd00::1'
   },
   websockets: {
-    host: 'vitasenior-ws.eu-gb.mybluemix.net'
-    //host: 'http://192.168.161.115:8008/'
+    host: 'vitasenior-ws.eu-gb.mybluemix.net/ws'
+    //host: 'http://192.168.161.115:8008/ws'
   },
   ServerConfigs: {
     key: fs.readFileSync('.key').toString().trim(),
