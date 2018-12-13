f27425bc635196a5ca7d70fad0f31cdba17d59e0
Nelson Gomes <nelsonmpg@gmail.com>
2018-12-13 20:24:51 +0000

    Update gitignore

--- Scripts/borderRouterRun.sh
+++ Scripts/borderRouterRun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 OUTLOGFILE="FOLDERVITABOX/logs/stdoutBorder.log"
 ERRLOGFILE="FOLDERVITABOX/logs/stderrBorder.log"
-rm "$OUTLOGFILE"
-rm "$ERRLOGFILE"
+# rm "$OUTLOGFILE"
+# rm "$ERRLOGFILE"
 cd "FOLDERROOT/contiki-ng/examples/rpl-border-router/"
 make TARGET=zoul savetarget >> "$OUTLOGFILE" 2>> "$ERRLOGFILE"
 make connect-router >> "$OUTLOGFILE" 2>> "$ERRLOGFILE"
