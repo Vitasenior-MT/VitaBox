3f0c3dca21eab6224c351912156a65296c41d859
Nelson Gomes <nelsonmpg@gmail.com>
2018-12-19 23:53:13 +0000

    Clean Branch VitaboxDevNG

--- Scripts/whatcherA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-NOW=$(date +"%Y-%m-%d")
-DATE=$(date +"%Y/%m/%d %H:%M:%S")
-LOGFILE="generatedLogs/log-$1-$NOW.log"
-echo "$(ps -C $1 -o pid,%cpu,%mem)" >> "$LOGFILE";
-echo "Date: $DATE" >> "$LOGFILE";
-sleep "$2";
-done
